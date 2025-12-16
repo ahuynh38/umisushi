b4bec6105b74eb4e0e1e24a2f15f6595581cef53
Andy Huynh <andy-huynh@hotmail.com>
2025-12-15 16:50:55 -0800

    Add dine in and takeout menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
+import DineInMenu from './components/DineInMenu';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dine-in-menu" element={<DineInMenu />} />
+      </Routes>
+    </Router>
   );
 }
 
